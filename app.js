3230ebf4482d0897d6d897cb1c4ca1f7f0d9b697
stevopablo <stevaopabllo@gmai.com>
2025-01-17 11:10:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const listaAmigos = document.getElementById('listaAmigos')
+const resultado = document.getElementById('resultado')
+
+
+function sortearAmigo(){
+
+}
